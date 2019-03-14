9328bd9ed765c641d3f30ea4a413ba30ed0c8b50
NicoleAMS <nicole.sentis@gmail.com>
2019-03-14 16:28:16 +0000

    adds scrollspy

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ $(function () {
 		$nav.toggleClass("nav-scrolled", $(this).scrollTop() > $nav.height());
   });
 });
+
+// Fluid scrollspy 
+$(function () {
+	$('header a').on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 1000, function(){
+			window.location.hash = hash;
+		});
+	});
+});
