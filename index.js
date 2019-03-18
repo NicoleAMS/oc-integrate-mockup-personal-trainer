6887e9800b2fcc7394516876ae7c0e83a2598c33
NicoleAMS <nicole.sentis@gmail.com>
2019-03-18 11:21:01 +0000

    adds spec sheet with client details/needs, updates few details according to client needs, adds modal with video in jumbotron, fixes some responsiveness issues

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ $(function () {
   });
 });
 
-// Fluid scrollspy 
+// Fluid scrollspy header navigation
 $(function () {
 	$('header a').on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 1000, function(){
+			window.location.hash = hash;
+		});
+	});
+});
+
+// Fluid scrollspy jumbotron button
+$(function () {
+	$('#classesBtn').on('click', function(e) {
 		e.preventDefault();
 		var hash = this.hash;
 		$('html, body').animate({
